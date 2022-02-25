5e5dd967f8f9b0561da702d4536eecca5490360a
Omar_Mohamady <74717586+Mohamady1@users.noreply.github.com>
2022-02-26 00:50:52 +0200

    Update shelf.jsx
    for unique id

--- src/components/shelf.jsx
+++ src/components/shelf.jsx
@@ -3,13 +3,15 @@ import Book from './book';
 
 function Shelf ({books, title, changeShelf}) {
   
+  let id = 21;
+  
     return (
         <div className="bookshelf">
             <h2 className="bookshelf-title">{title}</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
                   {books.map(book => (
-                      <Book key={book.id} book={book} changeShelf={changeShelf} />
+                      <Book key={++id} book={book} changeShelf={changeShelf} />
                   ))}
               </ol>
             </div>
